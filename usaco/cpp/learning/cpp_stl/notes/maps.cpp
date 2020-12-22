31ef0e3bc4f20b457befd336ec920fcc1a255221
TanishT <tanishtyagi@gmail.com>
2020-12-22 13:32:21 -0500

    traversing map
    traversing map

--- usaco/cpp/learning/cpp_stl/notes/maps.cpp
+++ usaco/cpp/learning/cpp_stl/notes/maps.cpp
@@ -7,11 +7,18 @@
 using namespace std;
 
 int main() {
-    map<int,int> A;
+    map<int,int> A; 
+    //key value pair, value in [] is key, val after equal sign is value
     A[1] = 100;
     A[2] = -1;
     A[3] = 200;
 
+    //traversing map
+    for (map<int,int>::iterator it = A.begin(); it != A.end(); it++) {
+        cout << it->first << " => " << it->second << endl;
+    }
+
+    //mass-adding values into map
     map<char,int> count;
     string x = "tanish tyaagi";
     for (char c : x) {
