abd787b5c95fb5cabb1d24dc0f6399d98d8b5de9
TanishT <tanishtyagi@gmail.com>
2020-12-22 12:15:50 -0500

    upper and lower bounds with sets
    upper and lower bounds with sets

--- usaco/cpp/learning/sets.cpp
+++ usaco/cpp/learning/sets.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 int main() {
-    //set stores number in ascending order, so all queries can be done in O(logn)
+    //set stores number in descending order, so all queries can be done in O(logn)
     //declaring a set
     set<int> S;
     S.insert(1);
@@ -26,6 +26,19 @@ int main() {
         cout << "not present" << endl;;
     } else {
         cout << *it << endl;
+    }
+
+    //upper and lower bound
+    //these are more efficent because values are sorted
+    //if not able to find, iterator value points to last element in set
+    auto it2 = S.lower_bound(-1); //first val that is greater than or equal to -1, in this case -1
+    auto it3 = S.upper_bound(-1); //first val that is greater than -1, in this case 1
+    if (it2 == S.end()) {
+        cout << "lower bound for -1 not present" << endl;
+    } else if (it3 == S.end()) {
+        cout << "upper bound for -1 not present" << endl;
+    } else {
+        cout << *it2 << " " << *it3 << endl;
     }
 
     return 0;
