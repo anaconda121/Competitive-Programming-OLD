e407007c996af133f81433fbca2704ab43bf2a4c
TanishT <tanishtyagi@gmail.com>
2020-12-24 12:22:31 -0500

    added custom comparator for an arbitary amount of criteria
    added custom comparator for an arbitary amount of criteria

--- usaco/cpp/learning/cpp_stl/notes/oop/structs.cpp
+++ usaco/cpp/learning/cpp_stl/notes/oop/structs.cpp
@@ -9,9 +9,26 @@ struct Edge {
     int a,b,w;
 };
 
+//equivalent code with a class
+/*class Edge {
+    public:
+        int a,b,w;
+};*/
+
 //comparator for sorting vector of edges, we choose to sort by w from least to greatest for this example
 bool cmp(const Edge& x, const Edge& y) { 
     return x.w < y.w; 
+    //return x.w > y.w for w from greatest - least
+}
+
+//comparator that has 2 criteria
+//this applies to an arbitary amount of criteria
+bool cmp2(const Edge& x, const Edge& y) {
+    if (x.w != y.w) {
+        return x.w < y.w; 
+    }
+    //if weights are equal, we go to parameter a for "tiebreaker"
+    return x.a < y.a;
 }
 
 void setIO(string name = "") { // name is nonempty for USACO file I/O
@@ -21,12 +38,6 @@ void setIO(string name = "") { // name is nonempty for USACO file I/O
     freopen((name+".out").c_str(), "w", stdout);
 }
 
-//equivalent code with a class
-/*class Edge {
-    public:
-        int a,b,w;
-};*/
-
 int main() {
     setIO("struct");
     int M = 4;
@@ -35,7 +46,8 @@ int main() {
         int a,b,w; cin >> a >> b >> w;
         v.push_back({a,b,w});
     }
-    sort(begin(v),end(v),cmp);
+    //sort(begin(v),end(v),cmp);
+    sort(begin(v), end(v), cmp2);
     for (Edge e: v) { 
         cout << e.a << " " << e.b << " " << e.w << "\n";
     }
