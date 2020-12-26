0ef51942dea83e05250ad9d441ce0308cd8b18d6
TanishT <tanishtyagi@gmail.com>
2020-12-26 13:51:44 -0500

    unzipped starter code and problem statements for cp course
    unzipped starter code and problem statements for cp course

---
+++ usaco/cpp/learning/coursera_course_docs/1-a-addition-and-subtraction/addition_and_subtraction.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    int x, y, z;
+    cin >> x >> y >> z;
+    int result = -1;
+
+    // your code
+
+    cout << result << endl;
+    return 0;
+}
