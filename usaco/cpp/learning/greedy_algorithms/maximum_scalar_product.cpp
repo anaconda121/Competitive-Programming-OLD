59343867a6a5d6f83d55e7fe3f12241b1d080e27
TanishT <tanishtyagi@gmail.com>
2020-12-26 13:40:25 -0500

    added a faster solution for maximum scalar product problem
    greedy algorithms

--- usaco/cpp/learning/greedy_algorithms/maximum_scalar_product.cpp
+++ usaco/cpp/learning/greedy_algorithms/maximum_scalar_product.cpp
@@ -4,6 +4,28 @@
 
 using namespace std;
 
+int maximum_scalar_productslow(vector<int> a, vector<int> b) {
+    sort(begin(a), end(a));
+    sort(begin(b), end(b));
+    int sum = 0;
+    for (int i = 0; i < a.size(); i++) {
+        sum += a[i]*b[i];
+    }
+    return sum;
+}
+
+int maximum_scalar_product(vector<int> a, vector<int> b) {
+    int ans = 0;
+    while (a.size()> 0) {
+        auto it = max_element(begin(a), end(a));
+        auto it2 = max_element(begin(b), end(b));
+        ans += *it * *it2;
+        a.erase(it);
+        b.erase(it2);
+    }
+    return ans;
+}
+
 int main() {
     int n;
     cin >> n;
@@ -15,12 +37,7 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> b[i];
     }
-    sort(begin(a), end(a));
-    sort(begin(b), end(b));
-    int sum = 0;
-    for (int i = 0; i < n; i++) {
-        sum += a[i]*b[i];
-    }
-    cout << sum << endl;
-    return 0;
+    int sum1 = maximum_scalar_product(a,b);
+    int sum2 = maximum_scalar_productslow(a,b);
+    cout << sum1 << " , " << sum2 << endl;
 }
