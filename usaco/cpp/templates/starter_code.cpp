cde97a2fa970301429535b1bb12ac7f7b733b848
TanishT <tanishtyagi@gmail.com>
2020-12-30 17:53:09 -0500

    starter code for any problem v1
    general addition

---
+++ usaco/cpp/templates/starter_code.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <vector>
+#include <math.h>
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+void setIO(string name, bool includeout=false) { // name is nonempty for USACO file I/O
+    ios_base::sync_with_stdio(0); cin.tie(0); // see Fast Input & Output
+    // alternatively, cin.tie(0)->sync_with_stdio(0);
+    freopen((name+".in").c_str(), "r", stdin); // see Input & Output
+    if (includeout) {
+        freopen((name+".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+    setIO("__placeholder__", true);
+    return 0;
+}
