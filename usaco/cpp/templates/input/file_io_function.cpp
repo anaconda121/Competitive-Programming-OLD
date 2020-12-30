bb91af94258ec76ab2995c36a1541c043b49302c
TanishT <tanishtyagi@gmail.com>
2020-12-30 17:50:56 -0500

    edited file/io function
    general fix

--- usaco/cpp/templates/input/file_io_function.cpp
+++ usaco/cpp/templates/input/file_io_function.cpp
@@ -2,9 +2,11 @@
 
 using namespace std;
 
-void setIO(string name = "") { // name is nonempty for USACO file I/O
+void setIO(string name, bool includeout=false) { // name is nonempty for USACO file I/O
     ios_base::sync_with_stdio(0); cin.tie(0); // see Fast Input & Output
     // alternatively, cin.tie(0)->sync_with_stdio(0);
     freopen((name+".in").c_str(), "r", stdin); // see Input & Output
-    freopen((name+".out").c_str(), "w", stdout);
+    if (includeout) {
+        freopen((name+".out").c_str(), "w", stdout);
+    }
 }
