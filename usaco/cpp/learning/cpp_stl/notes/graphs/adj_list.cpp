2b0a6a44618364d2e05072998617f70bd017d737
TanishT <tanishtyagi@gmail.com>
2021-01-13 22:47:29 -0500

    added code for adj matrix
    graphssssssssssssssssssss

--- usaco/cpp/learning/cpp_stl/notes/graphs/adj_list.cpp
+++ usaco/cpp/learning/cpp_stl/notes/graphs/adj_list.cpp
@@ -13,6 +13,20 @@ These nodes are called outgoing neighbours of node ii.
 (LOOK AT adj_lists.png)
 */
 
+/*PRO=CON 
+Advantages
+For a graph with NN nodes and MM edges, the memory used depends on M. This makes the adjacency lists ideal for storing sparse graphs.
+Let's assume we have a directed graph with 5\ 0005 000 nodes and (only) 10\ 00010 000 edges. In the case of the adjacency matrix, we would have allocated 25\ 000\ 00025 000 000 elements, whereas in the case of adjacency lists we only need 10\ 00010 000.
+For a given node finding all its neighbours requires checking only the node's list which contains a numbers of elements equal to the number of neighbours (one per neighbour).
+Adding an edge can be done easily, by appending the neighbour(s) in the corresponding list(s).
+
+Disadvantages
+Edge lookup is slow, in the worst case being necessary to traverse all the elements of a node's list.
+Erasing an edge is just as slow as the lookup.
+If the graph is dense, the adjacency lists are probably not as good as an adjacency matrix.
+Implementing them is slightly more difficult than an adjacency matrix. Even so, we still recommend them in most of the cases.
+*/
+
 class Graph {
     int N; //num of edges
     vector<int> *adj; //adjacency list
---
+++ usaco/cpp/learning/cpp_stl/notes/graphs/adj_matrix.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+#define N 4 // num nodes in graph
+
+/* PRO-CON 
+Advantages
+Edge lookup, i.e. checking if a particular edge exists, is done easily.
+Adding new edges, or deleting existing ones, is also straightforward.
+If the graph is dense, i.e. the number of edges is large. This is usually the preferred method of representing the graph.
+Certain algorithms are easier to implement using an adjacency matrix. 
+
+Disadvantages
+The memory allocated is always an N * NN∗N matrix, even in the case of sparse graphs, i.e. the number of edges is small.
+For a given node finding all its neighbours, i.e. other nodes that share an edge with it, requires checking an entire row of elements in the matrix.
+*/
+void addEdge(int graph[N][N], int u, int v){
+    //1. assuming that edge is undirected (bidirectional)
+    //2. assuming that values in tree go from 0 - (n-1)
+    graph[u][v] = 1; // u is adj to v
+    graph[v][u] = 1; // v is adj to u
+}
+
+void printAdjMatrix(int graph[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << graph[i][j] << " , ";
+        }
+        cout << "\n";
+    }
+}
+
+int main(){
+    int adj_matrix[N][N] = {0};
+    addEdge(adj_matrix, 0, 1);
+    addEdge(adj_matrix, 0, 3);
+    addEdge(adj_matrix, 1, 3);
+    addEdge(adj_matrix, 1, 2);
+    addEdge(adj_matrix, 2, 3);
+    printAdjMatrix(adj_matrix);
+    return 0;
+}
