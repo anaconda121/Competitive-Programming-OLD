7b26d3342facc795a7c726a053fc7f31a35ea877
TanishT <tanishtyagi@gmail.com>
2021-02-09 23:32:08 -0500

    fiddling around with milkvisits sol

--- usaco/silver/dec_2019/3/milkvisits_2.cpp
+++ usaco/silver/dec_2019/3/milkvisits_2.cpp
@@ -1,32 +1,33 @@
-//http://www.usaco.org/index.php?page=viewproblem2&cpid=968
 #include <bits/stdc++.h>
 
 using namespace std;
 
 #define maxn 100005
 
-int n, m, cc;
-int components[maxn];
-string s;
-char pref[maxn];
-vector<int> adj[maxn];
+int n, m, cc; // cc = componentCount
+int components[maxn]; //stores what component each farm is part of
+string s; //holds cow breed preferences for each farmer
+char pref[maxn]; //char array based of s since we are using 1-index system
+vector<int> adj[maxn]; //adjacency list for representing graph
 
-void setIO(string name, bool includeout=false) { // name is nonempty for USACO file I/O
-    ios_base::sync_with_stdio(0); cin.tie(0); // see Fast Input & Output
-    // alternatively, cin.tie(0)->sync_with_stdio(0);
-    freopen((name+".in").c_str(), "r", stdin); // see Input & Output
+//input function
+void setIO(string name, bool includeout=false) { 
+    ios_base::sync_with_stdio(0); cin.tie(0); 
+    freopen((name+".in").c_str(), "r", stdin); 
     if (includeout) {
         freopen((name+".out").c_str(), "w", stdout);
     }
 }
 
 void dfs(int start) {
-    if (components[start]) {
+    if (components[start] != 0) {
+		//current farm has already been assigned a component b/c it has already been visited
         return;
     }
-    components[start] = cc; //assign component #
+    components[start] = cc; //assign component # to farm
     for (auto next : adj[start]) {
         if (pref[start] == pref[next]) {
+			//found another farm to add to our current component
             dfs(next);
         }
     }
@@ -40,27 +41,26 @@ int main() {
     for (int i = 1; i < n+1; i++) {
         pref[i] = s[i-1];
     }
+	//creating graph
     for (int i = 0; i < n-1; i++) {
         int a, b;
         cin >> a >> b;
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
-    for (int i = 1; i < n+1; i++) if (!components[i]) {
-        //add new component
-        cc++;
-        dfs(i);
-    }
-    
-    /*for (int i = 1; i < n+1; i++) {
-        cout << components[i] << endl;
-    }*/
+	//creating connected components
+    for (int i = 1; i < n+1; i++) {
+		if (components[i] == 0) {
+			//add new component
+			cc++;
+			dfs(i);
+    	}
+	}
     for (int i = 0; i < m; i++) {
         int a, b;
         char c;
         cin >> a >> b >> c;
         if ((components[a] == components[b] && pref[a] == c) || components[a] != components[b]) {
-            //cout << pref[a] << " " << pref[b] << " " << c << endl;
             ans += '1';
         } else {
             ans += '0';
