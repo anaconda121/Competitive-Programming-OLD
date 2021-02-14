356b3c3490d954865bbeda82817068451a121545
TanishT <tanishtyagi@gmail.com>
2021-02-14 14:59:41 -0500

    moo particle sol

--- usaco/silver/us_2020/3/3.cpp
+++ usaco/silver/us_2020/3/3.cpp
@@ -1,18 +1,41 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-void setIO(string name, bool includeout=false) { // name is nonempty for USACO file I/O
-    ios_base::sync_with_stdio(0); cin.tie(0); // see Fast Input & Output
-    // alternatively, cin.tie(0)->sync_with_stdio(0);
-    freopen((name+".in").c_str(), "r", stdin); // see Input & Output
-    if (includeout) {
-        freopen((name+".out").c_str(), "w", stdout);
-    }
-}
+ifstream fin("3.in");
+ofstream fout("3.out");
+
+int n;
+pair<int,int> xy[100001];
+vector<int> lowerbounds;
 
 int main() {
-    setIO("3");
-    return 0;
+  fin >> n;
+  for (int i = 0; i < n; i++) {
+    fin >> xy[i].first >> xy[i].second;
+  }
+  sort(xy, xy + n);
+  lowerbounds.push_back(xy[0].second);
+  for (int i = 1; i < n; i++) {
+    int y = xy[i].second;
+    vector<int>::iterator itr = upper_bound(lowerbounds.begin(), lowerbounds.end(), y);
+    if (itr == lowerbounds.begin()) {
+      lowerbounds.insert(lowerbounds.begin(), y );
+    }
+    else {
+      itr--;
+      while (itr != lowerbounds.begin()) {
+        lowerbounds.erase(itr--);
+      }
+    }
+    for (int x : lowerbounds) {
+        fout << x << " , ";
+    }
+    fout << endl;
+  }
+  fout << lowerbounds.size() << endl;
+  return 0;
 }
