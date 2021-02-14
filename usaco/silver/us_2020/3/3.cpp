6e1c721498afb89c26dd6fd0eb62eb51a1de5e84
TanishT <tanishtyagi@gmail.com>
2021-02-13 21:11:56 -0500

    no clue what happened

---
+++ usaco/silver/us_2020/3/3.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+void setIO(string name, bool includeout=false) { // name is nonempty for USACO file I/O
+    ios_base::sync_with_stdio(0); cin.tie(0); // see Fast Input & Output
+    // alternatively, cin.tie(0)->sync_with_stdio(0);
+    freopen((name+".in").c_str(), "r", stdin); // see Input & Output
+    if (includeout) {
+        freopen((name+".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+    setIO("3");
+    return 0;
+}
