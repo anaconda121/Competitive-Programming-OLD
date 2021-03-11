848bd52b54d5c7e0ff26f341084e0d5a8edecd10
TanishT <tanishtyagi@gmail.com>
2021-03-11 08:54:32 -0500

    solved http://www.usaco.org/index.php?page=viewproblem2&cpid=570

--- solutions/usaco/silver/dec_2015/1/1.cpp
+++ solutions/usaco/silver/dec_2015/1/1.cpp
@@ -1,6 +1,7 @@
 /*LOGIC
+http://www.usaco.org/index.php?page=viewproblem2&cpid=570
 
-boolean matrix 
+logic is in comments all around code
 */
 
 #include <bits/stdc++.h>
@@ -14,11 +15,13 @@ using namespace std;
 #define maxn 105
 
 int n,m;
-bool grid[maxn][maxn];
+int xArr[4] = {0,0,-1,1};
+int yArr[4] = {1,-1,0,0};
+bool lit[maxn][maxn];
 bool visited[maxn][maxn];
 long long area = 0;
-long long ans = INT_MIN;
-
+long long ans = 1;
+vector<vector<vector<pair<int,int>>>> lights (maxn, vector<vector<pair<int, int>>>(maxn, vector<pair<int, int>>()));
 
 void setIO(string name, bool includeout=false) { // name is nonempty for USACO file I/O
     ios_base::sync_with_stdio(0); cin.tie(0); // see Fast Input & Output
@@ -29,31 +32,47 @@ void setIO(string name, bool includeout=false) { // name is nonempty for USACO f
     }
 }
 
-void floodfill(vector<vector<vector<pair<int,int>>>> lights, int r, int c){
+void floodfill(int r, int c){
 	if(r < 0 || r >= n || c < 0 || c >= n) return; // if outside grid
 	if(visited[r][c]) return; // already visited this square
-    if(!grid[r][c]) return; //not true, can't move from here to anywhere else
+    if(!lit[r][c]) return; //bessie can only travel through lit rooms
 	visited[r][c] = true; // mark current square as visited
     for (auto x : lights[r][c]) {
-        
+        if (!lit[x.f][x.s]) {
+            lit[x.f][x.s] = true;
+            ans++;
+            for (int i = 0; i < 4; i++) {
+                //range check
+                if (x.f + xArr[i] >= n || x.f + xArr[i] < 0) continue;
+				if (x.s + yArr[i] >= n || x.s + yArr[i] < 0) continue;
+                // if dood is visited, that must mean could be some more light switches that need to be toggled
+                // so go over there and check it out
+                // have to use visited here instead of lit since dood who can toggle lit switches but isn't toggled by anyone
+                // will only have visited = true and not lit. this is bad because dood could have ability to toggle multiple
+                // which isnt getting leveraged.
+                int row = x.f + xArr[i];
+                int col = x.s + yArr[i];
+                if (visited[row][col]) {
+                    floodfill(x.f, x.s);
+                }
+            }
+        }
     }
 	//recursively call floodfill for neighboring squares
-	floodfill(lights, r, c+1);
-	floodfill(lights, r, c-1);
-	floodfill(lights, r-1, c);
-	floodfill(lights, r+1, c);
+	for (int i = 0; i < 4; i++) {
+        floodfill(r + xArr[i], c + yArr[i]);
+    }
 }
 
 int main() {
-    setIO("lightson");
+    setIO("lightson", true);
     cin >> n >> m;
-    vector<vector<vector<pair<int,int>>>> lights (n, vector<vector<pair<int, int>>>(n, vector<pair<int, int>>()));
+    lit[0][0] = true;
     for (int i = 0; i < m; i++) {
         int x,y, x2, y2;
         cin >> x >> y >> x2 >> y2;
         x--; y--; x2--; y2--; 
         lights[x][y].push_back({x2,y2});
-        grid[x][y] = grid[x2][y2] = true;
     }
     // for (auto i : lights) {
     //     for (auto j : i) {
@@ -63,15 +82,7 @@ int main() {
     //          cout << endl;
     //     }
     // }
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (grid[i][j] && !visited[i][j]) {
-                floodfill(lights,i,j);
-                ans = max(ans, area);
-                area = 0;
-                cout << ans << endl;
-            }
-        }
-    }
+    floodfill(0,0);
+    cout << ans << endl;
     return 0;
 }
